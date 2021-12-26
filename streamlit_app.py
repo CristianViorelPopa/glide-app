4d92ac3ee849cfdce875d238679d0c1a4085f4f4
CristianViorelPopa <cristian.viorel.popa@gmail.com>
2021-12-26 02:42:39 +0200

    Checking

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ import streamlit as st
 # from IPython.display import display
 # import torch as th
 
-# from glide_text2im.download import load_checkpoint
-# from glide_text2im.model_creation import (
-#     create_model_and_diffusion,
-#     model_and_diffusion_defaults,
-#     model_and_diffusion_defaults_upsampler
-# )
+from glide_text2im.download import load_checkpoint
+from glide_text2im.model_creation import (
+    create_model_and_diffusion,
+    model_and_diffusion_defaults,
+    model_and_diffusion_defaults_upsampler
+)
 #
 # device = 'cpu'
 #
